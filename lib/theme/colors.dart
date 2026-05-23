86cc251e2fee1ed3e18b1501a38b3766c563a25d
wfanabila <wfnabila77@gmail.com>
2026-05-23 22:32:21 +0800

    Initial commit

---
+++ lib/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor customColor = MaterialColor(
+  0xFFFFCAA3,
+  <int, Color>{
+    50: Color(0xFFFFFFFF),
+    100: Color(0xFFFFE0D1),
+    200: Color(0xFFFF7E20),
+  },
+);
